dbfd2045638eb3da22cd1e01947e293ba4e205a7
hugosenari <hugosenari@gmail.com>
2022-03-23 23:02:37 -0300

    feat(nimpkgs): remove lock

--- v0_4_0/flake.nix
+++ v0_4_0/flake.nix
@@ -1,5 +1,5 @@
 {
-  description = ''Tool to clean up filenames shared on Dropbox'';
+  description = ''Mangles filename for dropbox compatibility.'';
 
   inputs.flakeNimbleLib.owner = "riinr";
   inputs.flakeNimbleLib.ref   = "master";
@@ -12,6 +12,22 @@
   inputs.src-dropbox_filename_sanitizer-v0_4_0.owner = "Araq";
   inputs.src-dropbox_filename_sanitizer-v0_4_0.repo  = "dropbox_filename_sanitizer";
   inputs.src-dropbox_filename_sanitizer-v0_4_0.type  = "github";
+  
+  inputs."nake".owner = "nim-nix-pkgs";
+  inputs."nake".ref   = "master";
+  inputs."nake".repo  = "nake";
+  inputs."nake".dir   = "1_9_4";
+  inputs."nake".type  = "github";
+  inputs."nake".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."nake".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."argument_parser".owner = "nim-nix-pkgs";
+  inputs."argument_parser".ref   = "master";
+  inputs."argument_parser".repo  = "argument_parser";
+  inputs."argument_parser".dir   = "";
+  inputs."argument_parser".type  = "github";
+  inputs."argument_parser".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."argument_parser".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
   
   outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
   let 
